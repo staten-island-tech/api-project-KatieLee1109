39c507eb39cf8e84bc3d20fbf4a7ef669701122c
KatieLee1109 <113190058+KatieLee1109@users.noreply.github.com>
2023-01-19 10:16:11 -0500

    commit

--- API Project/js/main.js
+++ API Project/js/main.js
@@ -1,5 +1,4 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
 
 const URL = "http://openlibrary.org/api/books?bibkeys=ISBN:0201558025,LCCN:93005405";
+
