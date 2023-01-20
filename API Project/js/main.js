3b0acfc1146f5c61381a26908e6e10aff4caddf9
KatieLee1109 <113190058+KatieLee1109@users.noreply.github.com>
2023-01-20 10:16:25 -0500

    commit

--- API Project/js/main.js
+++ API Project/js/main.js
@@ -1,4 +1,13 @@
-import './style.css'
-
-const URL = "http://openlibrary.org/api/books?bibkeys=ISBN:0201558025,LCCN:93005405";
+import "../style.css";
+const URL = "http://dog-api.kinduff.com/api/facts";
+async function getData(URL) {
+    try {
+        const response = await fetch(URL);
+        const data = await response.json();
+        document.getElementById("api-response").textContent = data.content;
+    } catch (error) {
+        console.log(error);
+    }
+}
+getData(URL);
 
