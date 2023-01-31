c3ef643ccd3e0375a30cb5a3f74b925c03e8cb27
KatieLee1109 <113190058+KatieLee1109@users.noreply.github.com>
2023-01-31 10:34:25 -0500

    Commit

--- API Project/js/main.js
+++ API Project/js/main.js
@@ -1,29 +1,3 @@
 import "../style.css";
-const URL = "https://api.lyrics.ovh/v1/artist/title";
-async function getData(URL) {
-    try {
-      const response = await fetch(URL);
-      const {} = await response.json();
-  
-      function display() {
-        forEach((word) => {
-          document.getElementById("api-response").insertAdjacentElement(
-            "afterbegin",
-            `<div class="card"
-            id="${word.name}">
-            <div class="songname"
-            id=${word.artist}">
-            <div class="artistname"
-            id="${word.lyrics}
-            </div>
-            `
-          );
-        });
-      }
-      display();
-    } catch (error) {
-      console.log(error);
-      alert("An error has occured");
-    }
-  }
-  getData(URL);
+const URL = "https://api.artic.edu/api/v1/artworks?fields=id,title,artist_display,date_display";
+console.log(URL);
