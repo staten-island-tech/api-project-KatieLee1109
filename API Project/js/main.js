d405bf4ae99ef857a229f5f725b4dc0384eb52ef
KatieLee1109 <113190058+KatieLee1109@users.noreply.github.com>
2023-01-31 22:58:11 -0500

    Final Commit

--- API Project/js/main.js
+++ API Project/js/main.js
@@ -1,3 +1,23 @@
 import "../style.css";
-const URL = "https://api.artic.edu/api/v1/artworks?fields=id,title,artist_display,date_display";
-console.log(URL);
+let input = document.querySelector(".query")
+let form = document.querySelector(".search")
+let p = document.querySelector(".display")
+
+form.addEventListener("click", (e)=> {
+    e.preventDefault();
+    let inputVal = input.value;
+    display(inputVal)
+});
+async function callAPI(value) {
+    let data = await fetch(`https://api.artic.edu/api/v1/artworks/search?q=${value}`);
+    let returned = await data.json();
+    return returned;
+};
+async function display(value) {
+    let data = await callAPI(value);
+    console.log(data)
+    data.data.forEach(element => {
+        p.insertAdjacentHTML('afterend', `<p>${element.title}</p>`)
+    });
+    p.innerHTML = data.data[0]
+};
